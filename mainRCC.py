f48618a49d5f23e8f5b45c81c2057a2f07f3a39c
Buffet Time <buffet-time@users.noreply.github.com>
2017-02-08 16:32:24 -0500

    reformatted script in places - added full loop

--- mainRCC.py
+++ mainRCC.py
@@ -20,27 +20,107 @@ GPIO.setup(BRAVO, GPIO.IN)
 print "Sensor 2 Online"
 print ""
 
-# don't edit anything above this comment
-
 # defining variables
-sensorSleep = 0.2       # change this for faster or slower output.
+sensorSleep = 0.1       # change this for faster or slower output.
 sensorSleep2 = 0.0001   # necessary buffer time - don't change this
 arrayOne = []           # array for sensor 1
 arrayTwo = []           # array for sensor 2
 counterOne = 0          # counter for loop in sensor 1
 counterTwo = 0          # counter for loop in sensor 2
+arraySize = 10          # the number of items to be averaged
 
-# the actual program
-print "Starting the While Loop"
+# ====================================
+#       loop to calculate average
+# ====================================
+
+print "Starting the loop to calculate average"
 print " "
-while True:  # infinite loop
+while True:  
 
-    # kills the loop after 100 iterations (change this later for testing purposes at the moment)
-    if counterTwo == 100:
+    # breaks the while loop after it reaches the integer in variable arraySize
+    if counterTwo == arraySize:
         break
     
     # ==============================================================================
-    # Sensor 1
+    #           Sensor 1
+    # ==============================================================================
+    
+    time.sleep(sensorSleep)
+    GPIO.output(TRIG, False)
+    time.sleep(sensorSleep2)
+    GPIO.output(TRIG, True)
+    time.sleep(sensorSleep2)
+    GPIO.output(TRIG, False)
+
+    while GPIO.input(ECHO) == 0:
+        pulseStartOne = time.time()
+
+    while GPIO.input(ECHO) == 1:
+        pulseEndOne = time.time()
+
+    # defining distance
+    pulseDurationOne = pulseEndOne - pulseStartOne
+    distanceOne = pulseDurationOne * 17150
+    distanceOne = round(distanceOne, 2)
+
+    # calculating distance for sensor 1
+    if True:
+        counterOne += 1
+        arrayOne.append(distanceOne) # adding each iteration to arrayOne
+
+    # =============================================================================
+    #           Sensor 2
+    # =============================================================================
+    
+    time.sleep(sensorSleep)
+    GPIO.output(DELTA, False)
+    time.sleep(sensorSleep2)
+    GPIO.output(DELTA, True)
+    time.sleep(sensorSleep2)
+    GPIO.output(DELTA, False)
+
+    while GPIO.input(BRAVO) == 0:
+        pulseStartTwo = time.time()
+
+    while GPIO.input(BRAVO) == 1:
+        pulseEndTwo = time.time()
+
+    # defining distance
+    pulseDurationTwo = pulseEndTwo - pulseStartTwo
+    distanceTwo = pulseDurationTwo * 17150
+    distanceTwo = round(distanceTwo, 2)
+
+    # calculating distance for sensor 2
+    if True:
+        counterTwo += 1
+        arrayTwo.append(distanceTwo) # adding each iteration to arrayTwo
+
+# =========================================
+#       end of loop to compute average
+# =========================================
+
+sumOne = sum(arrayOne)
+averageOne = sumOne/arraySize
+
+sumTwo = sum(arrayTwo)
+averageTwo = sumTwo/arraySize
+
+percentageAverageOne = averageOne / 10 * 4      # 40% of the average
+percentageAverageTwo = averageTwo / 10 * 4      # 40% of the average
+
+print str(percentageAverageOne)
+print str(percentageAverageTwo)
+print ''
+
+
+# ===================
+#    True loop  
+# ===================
+
+while True:
+    
+    # ==============================================================================
+    #           Sensor 1
     # ==============================================================================
     
     time.sleep(sensorSleep)
@@ -62,15 +142,13 @@ while True:  # infinite loop
     distanceOne = round(distanceOne, 2)
 
     # printing the distance of sensor 1
-    if 1 < distanceOne < 10000:
-        counterOne = counterOne + 1
+
+    if True:
+        counterOne += 1
         print '1)', distanceOne
-        arrayOne.append(distanceOne) # adding each iteration to arrayOne
-    else:
-        print "Out of Range - 1"
 
     # =============================================================================
-    # Sensor 2
+    #           Sensor 2
     # =============================================================================
     
     time.sleep(sensorSleep)
@@ -92,21 +170,7 @@ while True:  # infinite loop
     distanceTwo = round(distanceTwo, 2)
 
     # printing the distance of sensor 2
-    if 1 < distanceTwo < 10000:
-        counterTwo = counterTwo + 1
-        print "2)", distanceTwo
-        print ' '
-        arrayTwo.append(distanceTwo) # adding each iteration to arrayTwo
-    else:
-        print "Out of Range - 2"
-        print ' '
-
-sumOne = sum(arrayOne)
-averageOne = sumOne/100
-print 'Sensor One Average:'
-print averageOne
-
-sumTwo = sum(arrayTwo)
-averageTwo = sumTwo/100
-print 'Sensor Two Average:'
-print averageTwo
+    if True:
+        counterTwo += 1
+        print '2)', distanceTwo
+        print ''
